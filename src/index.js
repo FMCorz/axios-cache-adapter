de24ad7b1dfad0ddf460e2ab623170ff7903eed4
RasCarlito <rascarlito@gmail.com>
2017-11-08 23:02:16 +0100

    Add feature to read stale cache when a network error occurs

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import omit from 'lodash/omit'
 import merge from 'lodash/merge'
 import isFunction from 'lodash/isFunction'
 
-import { key } from './cache'
+import { key, read } from './cache'
 import MemoryStore from './memory'
 import request from './request'
 
@@ -25,6 +25,7 @@ const defaults = {
     adapter: axios.defaults.adapter,
     clearOnStale: true,
     clearOnError: true,
+    readOnError: false,
     debug: false
   },
   axios: {
@@ -43,6 +44,9 @@ const defaults = {
 function setupCache (config = {}) {
   // Extend default configuration
   config = merge({}, defaults.cache, config)
+
+  // Watch out for configuration conflicts
+  if (config.readOnError) config.clearOnStale = false
 
   // Create a cache key method
   config.key = key(config)
@@ -63,14 +67,32 @@ function setupCache (config = {}) {
   async function adapter (req) {
     // Execute request against local cache
     let res = await request(config, req)
-    const next = res.next
+
+    const requestConfig = res.config
+    let next = res.next
 
     // Response is not function, something was in cache, return it
     if (!isFunction(next)) return next
 
     // Nothing in cache so we execute the default adapter or any given adapter
     // Will throw if the request has a status different than 2xx
-    res = await config.adapter(req)
+    try {
+      res = await config.adapter(req)
+    } catch (err) {
+      if (config.readOnError) {
+        // Force cache tu return stale data
+        requestConfig.acceptStale = true
+
+        // Try to read from cache again
+        res = await request(requestConfig, req)
+
+        // Signal that data is from stale cache
+        res.next.request.stale = true
+
+        // No need to check if `next` is a function just return cache data
+        return res.next
+      }
+    }
 
     // Process response to store in cache
     return next(res)
